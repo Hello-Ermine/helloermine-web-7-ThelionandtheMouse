219470e98a5b485c4c09d1ead2536f778c8644ad
phawitpp <phawit.monc@kmutt.ac.th>
2021-11-08 19:50:45 +0700

    Add styled-component to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Navbar } from "react-bootstrap";
 function App() {
   return (
     <>
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="#"> 
+              TEST
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container fluid></Container>
     </>
   );
 }
