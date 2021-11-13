8701cafbd91dc18b5449b4693ae8c5c30e625eae
phawitpp <phawit.monc@kmutt.ac.th>
2021-11-13 14:48:26 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Navbar } from "react-bootstrap";
 import styled from 'styled-components';
