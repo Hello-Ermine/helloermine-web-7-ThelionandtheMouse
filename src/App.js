c23795c02b6d1cde65d3079f3950198d12e35f63
phawitpp <phawit.monc@kmutt.ac.th>
2021-11-13 15:05:24 +0700

    add sass + navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Navbar } from "react-bootstrap";
-import styled from 'styled-components';
+import { Container, Navbar, Nav } from "react-bootstrap";
+import styled from "styled-components";
 function App() {
   return (
     <>
-      ///Content go here
+      <Navbar variant="light" style={{ backgroundColor: "white", color: "black" }}>
+        <Container fluid>
+          <Navbar.Brand>
+            <Nav.Link href="#" style={{ float: "right", color: "black" }}>
+              <h6>Logo</h6>
+            </Nav.Link>
+          </Navbar.Brand>
+          <Nav.Link href="#" style={{ float: "right", color: "black" }}>
+            <h6>go to concluding</h6>
+          </Nav.Link>
+        </Container>
+      </Navbar>
     </>
   );
 }
