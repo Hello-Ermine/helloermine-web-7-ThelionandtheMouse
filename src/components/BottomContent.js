31eea68e4d114aa5f9e534d31de0dad6072f4088
csdean <unconfigured@null.spigotmc.org>
2021-11-17 20:54:33 +0700

    Modified BottomContent.js

--- src/components/BottomContent.js
+++ src/components/BottomContent.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import { Container, Row, Col } from "react-bootstrap";
-import avatar from './img/avatar.jpg';
+import avatar1 from './img/members/1.png';
+import avatar2 from './img/members/2.png';
+import avatar3 from './img/members/3.png';
+import avatar4 from './img/members/4.png';
+import avatar5 from './img/members/5.png';
+import avatar6 from './img/members/6.png';
 import AOS from 'aos';
 const  Lastsection = styled.div`
     font-family: 'Mitr', sans-serif;
@@ -39,7 +44,17 @@ h2{
 
 const roundedimg = {
 width: "100%",
-borderRadius: "50%",
+};
+
+const roundedsquare1 = {
+width: "200px",
+fontSize: "20px",
+borderRadius: "20px",
+};
+
+const roundedsquare2 = {
+width: "200px",
+borderRadius: "20px",
 };
 
 const BottomCon = () =>{
@@ -54,20 +69,28 @@ const BottomCon = () =>{
     <Container className="team" data-aos={"zoom-in"} data-aos-duration={"1000"}>
         <Col className='col-12 text-center'><h1>ผู้จัดทำ</h1></Col>
         <Row style={{marginTop:"2%"}}>
-            <Col className='col-2'><img src={avatar} alt="avatar" style={roundedimg}/></Col>
-            <Col className='col-2'><img src={avatar} alt="avatar" style={roundedimg}/></Col>
-            <Col className='col-2'><img src={avatar} alt="avatar" style={roundedimg}/></Col>
-            <Col className='col-2'><img src={avatar} alt="avatar" style={roundedimg}/></Col>
-            <Col className='col-2'><img src={avatar} alt="avatar" style={roundedimg}/></Col>
-            <Col className='col-2'><img src={avatar} alt="avatar" style={roundedimg}/></Col>
+            <Col className='col-2'><img src={avatar1} alt="Sand" style={roundedimg}/></Col>
+            <Col className='col-2'><img src={avatar2} alt="Bam" style={roundedimg}/></Col>
+            <Col className='col-2'><img src={avatar3} alt="Peem" style={roundedimg}/></Col>
+            <Col className='col-2'><img src={avatar4} alt="Pui" style={roundedimg}/></Col>
+            <Col className='col-2'><img src={avatar5} alt="Dean" style={roundedimg}/></Col>
+            <Col className='col-2'><img src={avatar6} alt="Jee" style={roundedimg}/></Col>
         </Row>
-        <Row className='text-center'>
-            <Col className='col-2'><p>ชื่อเล่น 001</p></Col>
-            <Col className='col-2'><p>ชื่อเล่น 001</p></Col>
-            <Col className='col-2'><p>ชื่อเล่น 001</p></Col>
-            <Col className='col-2'><p>ชื่อเล่น 001</p></Col>
-            <Col className='col-2'><p>ชื่อเล่น 001</p></Col>
-            <Col className='col-2'><p>ชื่อเล่น 001</p></Col>
+        <Row className='text-center' style={{marginTop:"3em"}}>
+            <Col className='col-2'><a className='btn btn-outline-light active' style={roundedsquare1}>แซนด์</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-light active' style={roundedsquare1}>แบม</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-light active' style={roundedsquare1}>ภีม</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-light active' style={roundedsquare1}>ปุ้ย</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-light active' style={roundedsquare1}>ดีน</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-light active' style={roundedsquare1}>จี้</a></Col>
+        </Row>
+        <Row className='text-center' style={{marginTop:".5em"}}>
+            <Col className='col-2'><a className='btn btn-outline-dark' style={roundedsquare2}>WEB DESIGN</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-dark' style={roundedsquare2}>WEB DESIGN</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-dark' style={roundedsquare2}>FRONT-END</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-dark' style={roundedsquare2}>FRONT-END</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-dark' style={roundedsquare2}>FRONT-END</a></Col>
+            <Col className='col-2'><a className='btn btn-outline-dark' style={roundedsquare2}>INFRASTRUCTURE</a></Col>
         </Row>
     </Container>
     </Lastsection>
